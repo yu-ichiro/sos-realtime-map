b5bcaaa0af8839ef4746cab2d734ab8503610562
Yuichiro Smith <contact@yu-smith.com>
2020-03-08 03:18:20 +0900

    :thinking_face: テスト

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
-
+import { ja, en } from "vuetify/src/locale";
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+    lang: {
+        locales: { ja, en },
+        current: 'ja'
+    }
+});
