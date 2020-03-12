90cc1a8414a4a7e0530d24762e9875167103de2d
Yuichiro Smith <contact@yu-smith.com>
2020-03-12 23:00:24 +0900

    :heavy_plus_sign: add Carter + add moves for Anna, Basil

--- src/assets/characters.ts
+++ src/assets/characters.ts
@@ -1,9 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { stateDependent } from "@/store/sos_state";
-import {
-  Place,
-  Places
-} from "@/assets/places";
+import { Place, Places } from "@/assets/places";
 
 export interface Character {
   name: string;
@@ -17,7 +14,7 @@ export const Characters: Record<string, Character> = {
     url: require("@/assets/img/characters/ann.jpg"),
     place: state => {
       if (state.sunny) {
-        if (state.date.readableValue.hour < 7) return Places.Hotel;
+        if (state.date.readableValue.hour < 7) return Places.HotelPrivate;
         else if (state.date.readableValue.hour < 10) return Places.Fountain;
         else if (state.date.readableValue.hour < 13) return Places.HotelSecond;
         else if (state.date.readableValue.hour < 16) return Places.Hotel;
@@ -38,27 +35,70 @@ export const Characters: Record<string, Character> = {
     name: "アンナ",
     url: require("@/assets/img/characters/anna.jpg"),
     place: state => {
-      if (state.sunny) {
-        if (state.date.readableValue.hour < 14) return Places.HouseBasil;
-        else if (state.date.readableValue.hour < 16) return Places.Square;
+      if (state.date.readableValue.hour < 7) return Places.HouseBasil;
+      else if (state.date.readableValue.hour < 10) {
+        if (state.date.readableValue.weekday === "Mon" && state.sunny)
+          return Places.MountainMiddle;
         else return Places.HouseBasil;
-      } else {
-        return Places.HouseBasil;
-      }
+      } else if (state.date.readableValue.hour < 13) return Places.HouseBasil;
+      else if (state.date.readableValue.hour < 16) {
+        if (state.date.readableValue.weekday === "Mon") return Places.MiscStore;
+        else if (state.sunny) return Places.Square;
+      } else if (state.date.readableValue.hour < 22) return Places.HouseBasil;
+      return Places.HouseBasilSecond;
     }
   },
   Barley: {
     name: "ムギ",
     url: require("@/assets/img/characters/barley.jpg"),
-    place: () => {
-      return Places.YodelRanch;
+    place: state => {
+      if (state.date.readableValue.hour < 8) return Places.YodelRanch;
+      else if (state.date.readableValue.hour < 13)
+        if (state.date.readableValue.weekday === "Mon")
+          return Places.HouseTomas;
+        else return Places.YodelRanchFirst;
+      else if (state.date.readableValue.hour < 16)
+        if (state.date.readableValue.weekday === "Mon" && state.sunny)
+          return Places.HotSpring;
+        else return Places.YodelRanchFirst;
+      else if (state.date.readableValue.hour < 19)
+        if (state.date.readableValue.weekday === "Mon" && !state.sunny)
+          return Places.YodelRanchSecond;
+        else return Places.YodelRanchFirst;
+      return Places.YodelRanchSecond;
     }
   },
   Basil: {
     name: "バジル",
     url: require("@/assets/img/characters/basil.jpg"),
-    place: () => {
-      return Places.HouseBasil;
+    place: state => {
+      if (state.date.readableValue.hour < 7) return Places.HouseBasilSecond;
+      else if (state.date.readableValue.hour < 10) {
+        if (state.date.readableValue.weekday === "Mon" && state.sunny)
+          return Places.MountainMiddle;
+        else return Places.HouseBasil;
+      } else if (state.date.readableValue.hour < 13) return Places.HouseBasil;
+      else if (state.date.readableValue.hour < 16) return Places.HouseBasilSecond;
+      else if (state.date.readableValue.hour < 19) return Places.HouseBasil;
+      else if (state.date.readableValue.hour < 22) return Places.Hotel;
+      return Places.HouseBasilSecond;
+    }
+  },
+  Carter: {
+    name: "カーター",
+    url: require("@/assets/img/characters/carter.jpg"),
+    place: state => {
+      if (state.date.readableValue.hour < 9) return Places.ChurchGrave;
+      else if (state.date.readableValue.hour < 13) return Places.Church;
+      else if (state.date.readableValue.hour < 16)
+        if (
+          state.date.readableValue.weekday === "Mon" ||
+          state.date.readableValue.weekday === "Wed" ||
+          !state.sunny
+        )
+          return Places.ChurchConfession;
+        else return Places.ChurchFront;
+      return Places.Church;
     }
   }
 };
--- src/assets/places.ts
+++ src/assets/places.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { stateCondition } from "@/store/sos_state";
+import { Characters } from "@/assets/characters";
 
 export interface Place {
   memo: string;
@@ -8,126 +9,156 @@ export interface Place {
   y: number;
 }
 
-export const Nowhere: Place = {
-  memo: "不明",
-  accessible: () => false,
-  x: -10000,
-  y: -10000
-};
-
-export const MyRanch: Place = {
-  memo: "牧場",
-  accessible: () => true,
-  x: 385,
-  y: 405
-};
-
-export const ChickenRanch: Place = {
-  memo: "にわとりりあ",
-  accessible: () => true,
-  x: 548,
-  y: 342
-};
-
-export const ChickenRanchPrivate: Place = {
-  memo: "にわとりりあ(2階)",
-  accessible: () => true,
-  x: 548,
-  y: 340
-};
-
-export const Smith: Place = {
-  memo: "鍛冶屋サイバラ",
-  accessible: () => true,
-  x: 425,
-  y: 275
-};
-
-export const MiscStore: Place = {
-  memo: "ザっか屋",
-  accessible: () => true,
-  x: 581,
-  y: 93
-};
-
-export const MiscStorePrivate: Place = {
-  memo: "ザっか屋(奥)",
-  accessible: () => true,
-  x: 581,
-  y: 93
-};
-
-export const Hotel: Place = {
-  memo: "ダッドの店",
-  accessible: () => true,
-  x: 560,
-  y: 174
-};
-
-export const HotelPrivate: Place = {
-  memo: "ダッドの店(奥)",
-  accessible: () => true,
-  x: 560,
-  y: 147
-};
-
-export const HotelSecond: Place = {
-  memo: "ダッドの店(２階)",
-  accessible: () => true,
-  x: 560,
-  y: 160
-};
-
-export const Fountain: Place = {
-  memo: "泉周辺",
-  accessible: () => true,
-  x: 242,
-  y: 360
-};
-
-export const Square: Place = {
-  memo: "広場",
-  accessible: () => true,
-  x: 784,
-  y: 252
-};
-
-export const HouseBasil: Place = {
-  memo: "バジルの家",
-  accessible: () => true,
-  x: 377,
-  y: 82
-};
-
-export const YodelRanch: Place = {
-  memo: "ヨーデル牧場",
-  accessible: () => true,
-  x: 685,
-  y: 350
-};
-
-export const YodelRanchYard: Place = {
-  memo: "ヨーデル牧場(庭)",
-  accessible: () => true,
-  x: 638,
-  y: 338
-};
-
 export const Places: Record<string, Place> = {
-  Nowhere: Nowhere,
-  MyRanch: MyRanch,
-  ChickenRanch: ChickenRanch,
-  ChickenRanchPrivate: ChickenRanchPrivate,
-  Smith: Smith,
-  MiscStore: MiscStore,
-  MiscStorePrivate: MiscStorePrivate,
-  Hotel: Hotel,
-  HotelPrivate: HotelPrivate,
-  HotelSecond: HotelSecond,
-  Fountain: Fountain,
-  Square: Square,
-  HouseBasil: HouseBasil,
-  YodelRanch: YodelRanch,
-  YodelRanchYard: YodelRanchYard
-}
-
+  Nowhere: {
+    memo: "不明",
+    accessible: () => false,
+    x: -10000,
+    y: -10000
+  },
+  MyRanch: {
+    memo: "牧場",
+    accessible: () => true,
+    x: 385,
+    y: 405
+  },
+  MountainMiddle: {
+    memo: "裏山(中腹)",
+    accessible: () => true,
+    x: 98,
+    y: 230
+  },
+  Fountain: {
+    memo: "泉周辺",
+    accessible: () => true,
+    x: 242,
+    y: 360
+  },
+  HotSpring: {
+    memo: "温泉",
+    accessible: () => true,
+    x: 221,
+    y: 442
+  },
+  Square: {
+    memo: "広場",
+    accessible: () => true,
+    x: 784,
+    y: 252
+  },
+  Church: {
+    memo: "教会",
+    accessible: () => true,
+    x: 783,
+    y: 93
+  },
+  ChurchGrave: {
+    memo: "教会墓地",
+    accessible: () => true,
+    x: 729,
+    y: 87
+  },
+  ChurchFront: {
+    memo: "教会前",
+    accessible: () => true,
+    x: 765,
+    y: 106
+  },
+  ChurchConfession: {
+    memo: "教会(懺悔室)",
+    accessible: state =>
+      Characters.Carter.place(state) === Places.ChurchConfession,
+    x: 758,
+    y: 61
+  },
+  Hotel: {
+    memo: "ダッドの店",
+    accessible: () => true,
+    x: 560,
+    y: 174
+  },
+  HotelPrivate: {
+    memo: "ダッドの店(奥)",
+    accessible: () => false,
+    x: 560,
+    y: 147
+  },
+  HotelSecond: {
+    memo: "ダッドの店(２階)",
+    accessible: () => true,
+    x: 560,
+    y: 160
+  },
+  HouseBasil: {
+    memo: "バジルの家",
+    accessible: () => true,
+    x: 377,
+    y: 82
+  },
+  HouseBasilSecond: {
+    memo: "バジルの家(2階)",
+    accessible: () => true,
+    x: 388,
+    y: 59
+  },
+  YodelRanch: {
+    memo: "ヨーデル牧場",
+    accessible: () => true,
+    x: 711,
+    y: 381
+  },
+  YodelRanchFirst: {
+    memo: "ヨーデル牧場(1階)",
+    accessible: () => true,
+    x: 685,
+    y: 350
+  },
+  YodelRanchSecond: {
+    memo: "ヨーデル牧場(2階)",
+    accessible: () => false,
+    x: 701,
+    y: 322
+  },
+  YodelRanchYard: {
+    memo: "ヨーデル牧場(庭)",
+    accessible: () => true,
+    x: 638,
+    y: 338
+  },
+  HouseTomas: {
+    memo: "町長の家",
+    accessible: () => true,
+    x: 490,
+    y: 83
+  },
+  ChickenRanch: {
+    memo: "にわとりりあ",
+    accessible: () => true,
+    x: 548,
+    y: 342
+  },
+  ChickenRanchPrivate: {
+    memo: "にわとりりあ(2階)",
+    accessible: () => true,
+    x: 548,
+    y: 340
+  },
+  Smith: {
+    memo: "鍛冶屋サイバラ",
+    accessible: () => true,
+    x: 425,
+    y: 275
+  },
+  MiscStore: {
+    memo: "ザっか屋",
+    accessible: () => true,
+    x: 581,
+    y: 93
+  },
+  MiscStorePrivate: {
+    memo: "ザっか屋(奥)",
+    accessible: () => true,
+    x: 581,
+    y: 93
+  }
+};
